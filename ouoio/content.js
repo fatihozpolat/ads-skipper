ab60f94dd37f7083dde5738c2dc579e4bec0ddfe
Fatih Özpolat <fozpolat33@gmail.com>
2023-01-09 23:03:15 +0300

    shortened

--- ouoio/content.js
+++ ouoio/content.js
@@ -1,29 +1,10 @@
 let url = window.location.href;
-if (url.indexOf("https://ouo.io/") > -1) {
+if (url.indexOf("https://ouo.io/") > -1 || url.indexOf('https://ouo.press/') > -1) {
     let urlParts = url.split("/");
 
     if (urlParts.length === 4) {
         let lastPart = urlParts[3];
-        window.location.href = 'https://ouo.io/fbc/' + lastPart;
-    } else if (urlParts.length === 5 && urlParts[3] === 'fbc') {
-        setTimeout(function () {
-            let form = document.getElementById('form-captcha');
-            form && form.submit();
-        }, 1000);
-    } else if (urlParts.length === 5 && urlParts[3] === 'go') {
-        setTimeout(function () {
-            let form = document.getElementById('form-go');
-            form && form.submit();
-        }, 1000);
-    }
-}
-
-// ouo.press
-if (url.indexOf('https://ouo.press/') > -1) {
-    let urlParts = url.split("/");
-    if (urlParts.length === 4) {
-        let lastPart = urlParts[3];
-        window.location.href = 'https://ouo.press/fbc/' + lastPart;
+        window.location.href = `https://${urlParts[2]}/fbc/${lastPart}`;
     } else if (urlParts.length === 5 && urlParts[3] === 'fbc') {
         setTimeout(function () {
             let form = document.getElementById('form-captcha');
